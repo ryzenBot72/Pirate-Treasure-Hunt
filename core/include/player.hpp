5116417dfabcb5d546d3a73abb0b3865cf1d933d
anushb-codes <bhandarianush55@gmail.com>
2025-11-18 21:31:04 +0530

    Update player.hpp

--- core/include/player.hpp
+++ core/include/player.hpp
@@ -1,16 +1,22 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+#include <vector>
+#include <string>
+#include "island.hpp" // Need this to know what a 'Clue' is
+
 using namespace std;
 
 typedef struct Inventory {
-    //Add items here
+    //MEMBER C UPDATE
+    // A dynamic list (Vector) to store collected clues.
+    // This represents a Linear Data Structure.
+    vector<Clue> collected_clues; 
+    // -----------------------
 } Inventory;
 
 typedef struct Player {
-    Inventory inventory;    //all inventory related stuff here
+    Inventory inventory;    
 } Player;
 
-
 #endif
-
