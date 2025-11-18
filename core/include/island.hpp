d5bbdda7579ce125239e206328e23bc9ba119041
anushb-codes <bhandarianush55@gmail.com>
2025-11-18 21:29:49 +0530

    Update island.hpp

--- core/include/island.hpp
+++ core/include/island.hpp
@@ -2,27 +2,40 @@
 #define ISLAND_HPP
 
 #include <vector>
+#include <string> // Required for string
 
 using namespace std;
 
+// MEMBER C UPDATE START
+// Defining the structure of a Clue
+typedef struct Clue {
+    int id;
+    string riddle;      // The puzzle text
+    string solution;    // The answer (optional, if you want input)
+    bool isFound;       // Has the player found this yet?
+} Clue;
+
 typedef struct Events {
-    //event type, all the relevent data
+    int type;           // 0 = nothing, 1 = Clue, 2 = Trap
+    string description;
+    Clue clueData;      // Embed the clue inside the event
 } Events;
+//MEMBER C UPDATE END
 
 typedef struct Items {
     //type of item, associated data 
+    string name; // Added name
 } Items;
 
 typedef struct IslandStuff {
-    vector<Events> events;  //challenges, direct hints
-    vector<Items> items;    //random items (along with their position maybe?)
+    vector<Events> events;  // Member C: Clues stored here
+    vector<Items> items;    
 } IslandStuff;
 
 typedef struct Island {
     int id;
-    int coords[2];          //x,y coordinates of the island
+    int coords[2];          
     IslandStuff initial;
 } Island;
 
 #endif
-
